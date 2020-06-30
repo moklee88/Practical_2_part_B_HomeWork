f27a84aa644e54d093b21186ceee8f0ccb649c0d
moklee88 <cuihaomok88@gmail.com>
2020-06-30 20:40:26 +0800

    practical 2 part b

--- app/src/main/java/com/example/practical_2_part_B/MainActivity.java
+++ app/src/main/java/com/example/practical_2_part_B/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.practical_2_part_B;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button mCountButton;
+    private Button mResetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
     public void countUp(View view){
         if(mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+
+        mResetButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        mResetButton.setClickable(true);
+
+        switch (mCount %2){
+            case 0: view.setBackgroundColor(Color.MAGENTA);
+            break;
+            case 1: view.setBackgroundColor(Color.GREEN);
+            break;
+            default:break;
+        }
+    }
+
+    public void resetZero(View view)
+    {
+        mCount=0;
+        mShowCount.setText(String.valueOf(mCount));
+
+        view.setClickable(false);
+        view.setBackgroundColor(Color.GRAY);
+        mCountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
     }
 }
